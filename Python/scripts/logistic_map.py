afa6ea525fd93d7de5462bfe289abca75852af36
Sean Mitchell <sm0077@uah.edu>
2021-05-15 12:32:58 -0500

    A few last changes

--- Python/scripts/logistic_map.py
+++ Python/scripts/logistic_map.py
@@ -1,17 +1,20 @@
-# logistic_map.py
+# Filename: logistic_map.py
+# Author: Sean Mitchell
 # short script to generate a text file with the logistics map
 
 import numpy as np
 
 length = 1600
+a = 3.900142000000020
+
 y_i = np.zeros(length)
 y_i[0] = 0.7920
 
 for j in range(1,length):
-    y_i[j] = (3.900142000000020 * y_i[j-1]) * (1 -  y_i[j-1])
+    y_i[j] = (a * y_i[j-1]) * (1 -  y_i[j-1])
 
-a = y_i.reshape(40,40)
-y_i_shaped = np.transpose(a)
+y_i_temp = y_i.reshape(40,40)
+y_i_shaped = np.transpose(y_i_temp)
 
 np.savetxt('../../datasets/logistic_map_raw.txt', y_i)
 np.savetxt('../../datasets/logistic_map_shaped.txt', y_i_shaped)
