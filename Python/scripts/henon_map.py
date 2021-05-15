5ba541534331115e1ed85d38c2d989231b2f0e8e
Sean Mitchell <sm0077@uah.edu>
2021-05-15 10:39:28 -0500

    Moved map generation scripts into new path

--- Python/scripts/henon_map.py
+++ Python/scripts/henon_map.py
@@ -16,8 +16,8 @@ for i in range(1,length):
     x_i[i] = 1 - alpha * x_i[i-1]**2 + y_i[i-1];
     y_i[i] = beta * x_i[i - 1];
 
-np.savetxt('../data/henon_map_x.txt', x_i)
-np.savetxt('../data/henon_map_y.txt', y_i)
+np.savetxt('../../datasets/data/henon_map_x.txt', x_i)
+np.savetxt('../../datasets/data/henon_map_y.txt', y_i)
 
 plt.plot(x_i,y_i,'.',markersize=1)
 plt.title('Henon Map')
---
+++ Python/scripts/logistic_map.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+length = 1600
+y_i = np.zeros(length)
+y_i[0] = 0.7920
+
+for j in range(1,length):
+    y_i[j] = (3.900142000000020 * y_i[j-1]) * (1 -  y_i[j-1])
+
+a = y_i.reshape(40,40)
+y_i_shaped = np.transpose(a)
+
+np.savetxt('../../datasets/logistic_map_raw.txt', y_i)
+np.savetxt('../../datasets/logistic_map_shaped.txt', y_i_shaped)
