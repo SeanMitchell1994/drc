758e082721f5a6ad020945e89fb6d9badc57a638
Sean Mitchell <sm0077@uah.edu>
2021-05-15 10:20:49 -0500

    Fixes and gitignore

--- Python/src/rc_run.py
+++ Python/src/rc_run.py
@@ -1,9 +1,9 @@
 from rc import *
 import matplotlib.pyplot as plt
 
-new_rc = RC(40,0.208)
-new_rc.Load_Reservoir_Data('../data/logistic_map_shaped.txt')
-new_rc.Load_Data('../data/MackeyGlass_t17.txt')
+new_rc = RC(40,0.35)
+new_rc.Load_Reservoir_Data('../../datasets/logistic_map_shaped.txt')
+new_rc.Load_Data('../../datasets/MackeyGlass_t17.txt')
 new_rc.Generate_Reservoir()
 new_rc.Train(2000)
 new_rc.Run_Generative(1000)
