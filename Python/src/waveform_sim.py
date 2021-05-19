31a79d95adda21aa8048b2a8189db04339ac8356
Sean Mitchell <sm0077@uah.edu>
2021-05-19 13:46:07 -0500

    Initial commit of waveform simulator

---
+++ Python/src/waveform_sim.py
@@ -0,0 +1,9 @@
+# Filename: waveform_sim.py
+# Author: Sean Mitchell
+# short script to generate a real time waveform
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
