f8b8faed00f9492631a590dfad50c5a7ebaf104f
Sean Mitchell <sm0077@uah.edu>
2021-05-21 21:35:33 -0500

    Code cleanup

--- Python/src/waveform_sim.py
+++ Python/src/waveform_sim.py
@@ -2,8 +2,24 @@
 # Author: Sean Mitchell
 # short script to generate a real time waveform
 
+import math
+import random
+import matplotlib.pyplot as plt
+
+def Sinewave(timestep):
+    x = math.sin(timestep) + random.randint(-3,3) + random.randint(-2,2) + random.randint(-1,1)
+    return x
+
 def main():
-    print("Hello World!")
+    #print("Hello World!")
+
+    b = []
+    for i in range(1,100):
+        a = Sinewave(i)
+        b.append(a)
+
+    plt.plot(b)
+    plt.show()
 
 if __name__ == "__main__":
     main()
