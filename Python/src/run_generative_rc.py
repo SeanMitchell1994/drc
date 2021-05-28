29df24751c6c0422ff31dd1e227568620fa5a8ae
Sean Mitchell <sm0077@uah.edu>
2021-05-28 14:03:40 -0500

    refactored rc_run into two files for ease of demonstration

--- Python/src/run_generative_rc.py
+++ Python/src/run_generative_rc.py
@@ -7,7 +7,6 @@ new_rc.Load_Data('../../datasets/MackeyGlass_t17.txt')
 new_rc.Generate_Reservoir()
 new_rc.Train(2000)
 new_rc.Run_Generative(1000)
-#new_rc.Run_Predictive(1000)
 new_rc.Get_MSE(500)
 
 silent_run = True
