51fdf020c3e0e3901e11ce4349cce7c5d3d37c70
Sean Mitchell <sm0077@uah.edu>
2021-06-24 08:35:02 -0500

    Working IC sweep

--- Python/src/experiment_initial_condition_sweep.py
+++ Python/src/experiment_initial_condition_sweep.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from multiprocessing import Process, Pool
 
-def lm_sweep(a, res_size):
+def lm_sweep(a, res_size, ic):
     length = res_size * res_size
 
     y_i = np.zeros(length)
-    y_i[0] = 0.7920
+    y_i[0] = ic
 
     for j in range(1,length):
         y_i[j] = (a * y_i[j-1]) * (1 -  y_i[j-1])
@@ -20,14 +20,15 @@ def lm_sweep(a, res_size):
 
 def main():
     param = 3.5
-    iterate = 2
+    iterate = 0.01
     res_size = 40
+    ic = 0.01
     mse_list = []
     param_list = []
-    while res_size <= 3200:
+    while ic <= 1:
         new_rc = RC(res_size,0.35)
         #new_rc.Load_Reservoir_Data('../../datasets/logistic_map_shaped.txt')
-        new_rc.rc_data  = lm_sweep(param,res_size)
+        new_rc.rc_data  = lm_sweep(param,res_size,ic)
         new_rc.Load_Data('../../datasets/MackeyGlass_t17.txt')
         new_rc.Generate_Reservoir()
         new_rc.Train(2000)
@@ -36,8 +37,8 @@ def main():
         mse = new_rc.Get_MSE()
 
         mse_list.append(mse)
-        param_list.append(res_size)
-        res_size = res_size * iterate
+        param_list.append(ic)
+        ic = ic + iterate
     
     plt.figure(1).clear()
     plt.plot( param_list,mse_list, linewidth=1 )
