60fee47a848bc0543c43d19c5f374a9620658237
Sean Mitchell <sm0077@uah.edu>
2021-06-23 20:37:27 -0500

    A lot of new things

--- Python/src/experiment_logistics_sweep.py
+++ Python/src/experiment_logistics_sweep.py
@@ -1,6 +1,7 @@
 from rc import *
 import matplotlib.pyplot as plt
 import numpy as np
+from multiprocessing import Process, Pool
 
 def lm_sweep(a, res_size):
     length = res_size * res_size
@@ -16,15 +17,8 @@ def lm_sweep(a, res_size):
 
     return y_i_shaped
 
-def main():
-    param = 3
-    iterate = 0.01
-    res_size = 40
-    mse_list = []
-    param_list = []
-
-
-    while param <= 4.0:
+def run_exp(param):
+        res_size = 40
         new_rc = RC(res_size,0.35)
         #new_rc.Load_Reservoir_Data('../../datasets/logistic_map_shaped.txt')
         new_rc.rc_data  = lm_sweep(param, res_size)
@@ -34,22 +28,35 @@ def main():
         new_rc.Run_Predictive(1000)
         new_rc.Compute_MSE(500)
         mse = new_rc.Get_MSE()
+        return (param,mse)
 
-        mse_list.append(mse)
-        param_list.append(param)
+def main():
+    param = 3
+    iterate = 0.0001
+    res_size = 40
+    values = []
+    while param <= 4.0:
+        values.append(param)
         param = param + iterate
 
+    pool = Pool(processes=8)
+    x,y = zip(*pool.map(run_exp, values))
+    
     plt.figure(1).clear()
-    plt.plot( param_list,mse_list, linewidth=1 )
+    plt.plot( x,y, linewidth=1 )
     plt.title('Logistics Map Parameter Sweep')
     plt.xlabel('a')
     plt.ylabel('MSE')
 
     # values for fixed points from https://mathworld.wolfram.com/LogisticMap.html
-    xcoords = [3, 3.82842712, 3.44948974, 3.73817237, 3.62655316, 3.54409035, 3.70164076, 3.56440726, 3.939200000003982, 3.774900000003636,3.685500000003447, 3.872300000003841,3.866200000003828,3.859600000003814,3.801700000003692,3.755000000003594,3.751400000003586,3.711700000003502,3.724800000003530,3.640300000003351, 3.640300000003351,3.605200000003277]
-    for xc in xcoords:
-        plt.axvline(x=xc, color='k', linestyle='--', linewidth=1)
-    plt.legend(['MSE','Fixed Points'],loc="lower left")
+    #xcoords = [3, 3.82842712, 3.44948974, 3.73817237, 3.62655316, 3.54409035, 3.70164076, 3.56440726, 3.939200000003982, 3.774900000003636,3.685500000003447, 3.872300000003841,3.866200000003828,3.859600000003814,3.801700000003692,3.755000000003594,3.751400000003586,3.711700000003502,3.724800000003530,3.640300000003351, 3.640300000003351,3.605200000003277]
+    even_fp = [3.540650000010093, 3.568640000010277, 3.571900000010298]
+    odd_fp = [3, 3.436550000009411, 3.5635300000102433, 3.569950000010285, 3.570210000010287, 3.570730000010290]
+    for xc in even_fp:
+        plt.axvline(x=xc, color='r', linestyle='--', linewidth=1)
+    for yc in odd_fp:
+        plt.axvline(x=yc, color='g', linestyle='--', linewidth=1)
+    plt.legend(['MSE','Even Fixed Points', 'Odd Fixed Points'],loc="lower left")
     plt.show()
 
 if __name__ == "__main__":
