917bb6d7f8348ebd662f91f21462574fefa9c715
Sean Mitchell <sm0077@uah.edu>
2021-08-15 16:24:59 -0500

    TCP test files

---
+++ Python/src/tcp_client.py
@@ -0,0 +1,15 @@
+import socket
+import time
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        s.sendall(b'Hello, world\n')
+        time.sleep(1)
+    #data = s.recv(1024)
+
+#print('Received', repr(data))
